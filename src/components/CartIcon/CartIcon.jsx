a3e1f3bb7ed427fe32c5ac474a23a8027ce1209c
Joshua Jeyaranjan <joshuajeyaranjan@macbookpro.lan>
2025-06-20 09:59:29 -0400

    remove typo in env import

--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './CartIcon.scss'
 
+const BUCKET_URL = process.meta.env.VITE_BUCKET_URL
+
 
 const CartIcon = () => {
   return (
@@ -9,7 +11,7 @@ const CartIcon = () => {
     <Link to='/cart'>
     <img 
     className="cart-icon nav__link nav__link--ig nav__ig " 
-    src="https://r2-image-proxy.r2-image-proxy.workers.dev/assets/cart.svg" 
+    src={`${BUCKET_URL}/assets/cart.svg`} 
     alt="Cart Icon" />
   
     </Link>
