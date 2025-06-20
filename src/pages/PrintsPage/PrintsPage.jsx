4afb0147c7fd28eb76a27aec019768de20153d0a
Joshua Jeyaranjan <joshuajeyaranjan@macbookpro.lan>
2025-06-20 13:53:00 -0400

    nav update

--- src/pages/PrintsPage/PrintsPage.jsx
+++ src/pages/PrintsPage/PrintsPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './PrintsPage.scss'
 import Gallery from '../../components/Gallery/Gallery'
 import Nav from '../../components/Nav/Nav'
@@ -6,9 +6,24 @@ import Footer from '../../components/Footer/Footer'
 import BackToTop from '../../components/BacktoTop/BacktoTop'
 
 const PrintsPage = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 350); // Loader visible for 250ms
+
+    return () => clearTimeout(timer); // Cleanup timer on unmount
+  }, []);
   return (
     <>
-    
+       {isLoading && (
+        <div className="page-loader">
+          {/* You can put a simple spinner or a subtle logo here if desired */}
+          {/* For now, it's just a full-screen overlay */}
+        </div>
+      )}
     <Nav/>
     <main>
        
