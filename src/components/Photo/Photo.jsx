9f1254c8c095317ecfb2dc8853fd946079c00d8f
Joshua Jeyaranjan <joshuajeyaranjan@macbookpro.lan>
2025-07-01 09:31:24 -0400

    updated photopage component to better replace broken avif with jpg

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Photo.scss';
-import useAvifSupport from '../../utils/useAvifSupport';
 
 const baseUrl = "https://media.joshuajeyphotography.com";
 
 function Photo({ id, filename, folder, alt, title }) {
   const [isLoaded, setIsLoaded] = useState(false);
-  const avifSupported = useAvifSupport();
 
   const avifSrc = `${baseUrl}/${folder}/${filename}.avif`;
   const webpSrc = `${baseUrl}/${folder}/${filename}.webp`;
@@ -19,20 +17,9 @@ function Photo({ id, filename, folder, alt, title }) {
 
   return (
     <Link to={`/photo/${id}`} className={`photo-item ${isLoaded ? 'loaded' : ''}`}>
-      {avifSupported ? (
-        <picture>
-          <source srcSet={avifSrc} type="image/avif" />
-          <source srcSet={webpSrc} type="image/webp" />
-          <source srcSet={jpgSrc} type="image/jpeg" />
-          <img
-            src={jpgSrc}
-            alt={alt}
-            className="photo-thumbnail"
-            loading="lazy"
-            onLoad={handleImageLoad}
-          />
-        </picture>
-      ) : (
+      <picture>
+        <source srcSet={avifSrc} type="image/avif" />
+        <source srcSet={webpSrc} type="image/webp" />
         <img
           src={jpgSrc}
           alt={alt}
@@ -40,7 +27,7 @@ function Photo({ id, filename, folder, alt, title }) {
           loading="lazy"
           onLoad={handleImageLoad}
         />
-      )}
+      </picture>
     </Link>
   );
 }
